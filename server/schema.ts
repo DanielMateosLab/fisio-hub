37217dd5126e1a244cdee5026946adf043a762e4
root <daniel.mat.lab@gmail.com>
2020-04-08 10:28:04 +0000

    Wrote an initial schema

--- server/schema.ts
+++ server/schema.ts
@@ -2,12 +2,51 @@ import { gql } from 'apollo-server'
 
 const typeDefs = gql`
   type Appointment {
-    id: ID
+    id: ID!
+    date: Int
+    proffesional: Proffesional
+    patient: Patient
+    status: AppointmentStatus
+    payment: Payment
   }
   type Service {
     id: ID!
     name: String
+    "Expressed in minutes"
+    duration: Int
     prize: Float
+  }
+  type Proffesional {
+    id: ID!
+    name: String
+    appointments: [Appointment]
+  }
+  type Patient {
+    id: ID!
+    name: String
+    appointments: [Appointment]
+  }
+  type Payment {
+    id: ID!
+    cash: Int
+    card: Int
+    appointment: Appointment
+  }
+
+  enum AppointmentStatus {
+    PENDING
+    MISSED
+    DONE
+  }
+
+  type Query {
+    "Fetch appointments by id"
+    fetchAppointments(id: ID!): [Appointment]
+  }
+  type Mutation {
+    createAppointment(): [Appointment]
+    updateAppointment(): [Appointment]
+    deleteAppointment(): [Appointment]
   }
 `
 
