d04afc633d4655e2034024d1937389ef1ce6124e
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-04-21 14:20:25 +0200

    Added custom types

---
+++ utils/types.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiHandler, NextApiResponse } from "next";
+import { Db } from "mongodb";
+
+export interface CustomApiRequest extends NextApiRequest {
+  db: Db
+}
+
+export type CustomApiHandler<T = any> = (
+  req: CustomApiRequest,
+  res: NextApiResponse<T>
+) => void 
+
+export interface ProffesionalInfo {
+  name: string
+  email: string
+  password: string
+}
