a16a271a194ae2cf624fa1db19f8baf0d927f199
danielmateoslab <danielmateoslab@github.com>
2020-04-24 19:57:25 +0200

    Create login api route. Added TODO

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -6,7 +6,7 @@ import ProffesionalsDAO from "../../storage/proffesionalsDAO";
 const handler: CustomApiHandler = (req: CustomApiRequest, res: NextApiResponse) => {
   ProffesionalsDAO.injectDB(req.db)
   // TODO: Validation with Yup (extract Yup validation schemas to utils)
-  res.status(400).json({ error: 'Mock error' })
+  res.status(400).json({ error: 'Mock Error' })
 }
 
 export default database(handler)
