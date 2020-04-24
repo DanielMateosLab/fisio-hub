c178d9b76ef8d508b314e5c66d92d54769cf88e6
danielmateoslab <danielmateoslab@github.com>
2020-04-24 19:58:03 +0200

    Updated forms to use Formik

--- components/login.tsx
+++ components/login.tsx
@@ -4,41 +4,65 @@ import CustomTextInput from './CustomTextInput'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import * as Yup from 'yup'
+import Typography from '@material-ui/core/Typography'
 
 const validationSchema = Yup.object({
   email: Yup.string().email('La dirección de correo electrónico no es válida').required('Obligatorio'),
   password: Yup.string().min(5, 'Debe tener 5 o más caracteres').required('Obligatorio')
 })
 
-const Login = () => (
-  <div>
-    <Formik
-      initialValues={{
-        email: '',
-        password: ''
-      }}
-      validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        setSubmitting(false)
-        console.log(JSON.stringify(values, null, 2))
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Grid container justify="center">
-            <CustomTextInput name="email" label="Correo electrónico" />
-            <CustomTextInput name="password" label="Contraseña" />
-            <Button
-              type="submit"
-              disabled={isSubmitting}
-              variant="contained"
-              color="primary"
-            > Iniciar sesión </Button>
-          </Grid>
-        </Form>
-      )}
-    </Formik>
-  </div>
-)
+const Login = () => {
+  let submitError: string
 
+  return(
+    <div>
+      <Formik
+        initialValues={{
+          email: '',
+          password: ''
+        }}
+        validationSchema={validationSchema}
+        onSubmit={async (values, {setSubmitting}) => {
+          try {
+            const res = await fetch('/api/login', {
+              method: 'post',
+              body: JSON.stringify(values)
+            })
+
+            const {error} = await res.json()
+            if (error) {
+              submitError = error
+            }
+          } catch (e) {
+            submitError = e.message
+          } finally {
+            setSubmitting(false)
+          }
+        }}
+      >
+        {({isSubmitting}) => (
+          <Form>
+            <Grid container justify="center">
+              <CustomTextInput name="email" label="Correo electrónico"/>
+              <CustomTextInput name="password" type="password" label="Contraseña"/>
+              {submitError && (
+                <Grid item style={{ marginTop: 18 }} container justify="center" xs={12}>
+                  <Typography color="error" variant="h6">{submitError}</Typography>
+                </Grid>
+              )}
+              <Grid item style={{ marginTop: 18 }} container justify="center" xs={12}>
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  variant="contained"
+                  color="primary"
+                > Iniciar sesión </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
 export default Login
--- components/register.tsx
+++ components/register.tsx
@@ -1,51 +1,87 @@
-import useForm from "../utils/useForm";
 import React from 'react'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import Grid from '@material-ui/core/Grid'
+import CustomTextInput from './CustomTextInput'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+
+const validationSchema = Yup.object({
+  firstName: Yup.string().max(30, 'Debe tener 30 o menos caracteres').required('Obligatorio'),
+  lastName: Yup.string().max(30, 'Debe tener 30 o menos caracteres').required('Obligatorio'),
+  email: Yup.string().email('La dirección de correo electrónico no es válida').required('Obligatorio'),
+  password: Yup.string().min(5, 'Debe tener 5 o más caracteres').required('Obligatorio'),
+  repeatPassword: Yup.string().required('Obligatorio')
+})
 
 const Register = () => {
-  const form = useForm(
-    {
-      email: {
-        type: "email",
-        label: "Email"
-      },
-      name: {
-        type: "text",
-        label: "Nombre"
-      },
-      password: {
-        type: "password",
-        label: "Contraseña"
-      },
-      repeatPassword: {
-        type: "password",
-        label: "Repite la contraseña",
-        validationFunction: (repeatPassword, { password }) => {
-          if (repeatPassword !== password) {
-            return 'Las contraseñas deben coincidir'
+  let submitError: string
+
+  return (
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        repeatPassword: ''
+      }}
+      validationSchema={validationSchema}
+      onSubmit={async (values, { setSubmitting }) => {
+        try {
+          const res = await fetch('/api/register', {
+            method: 'post',
+            body: JSON.stringify(values)
+          })
+
+          const { error } = await res.json()
+          if (error) {
+            submitError = error
           }
+        } catch (e) {
+          submitError = e.message
         }
-      }
-    },
-    async (results): Promise<void | string> => {
-      try {
-        delete results.repeatPassword
-
-        const res = await fetch('/api/register', {
-          method: 'post',
-          body: JSON.stringify(results)
-        })
-
-        const {error} = await res.json()
-        if (error) return error
-        return
-      } catch (e) {
-        return e.message
-      }
-    },
-    'Registrarme'
+        finally {
+          setSubmitting(false)
+        }
+      }}
+    >
+      {({ isSubmitting, values }) => (
+        <Form>
+          <Grid container justify="center">
+            <CustomTextInput name="firstName" label="Nombre" />
+            <CustomTextInput name="lastName" label="Apellidos" />
+            <CustomTextInput name="email" label="Correo electrónico" />
+            <CustomTextInput name="password" type="password" label="Contraseña" />
+            <CustomTextInput
+              name="repeatPassword"
+              label="Repite la contraseña"
+              type="password"
+              validate={() => {
+                if (values.password !== values.repeatPassword) {
+                  return 'Las contraseñas deben coincidir'
+                }
+              }}
+            />
+            { submitError && (
+              <Grid item style={{ marginTop: 18 }} container justify="center" xs={12}>
+                <Typography color="error" variant="h6">{submitError}</Typography>
+              </Grid>
+            )}
+            <Grid item style={{ marginTop: 18 }} container justify="center" xs={12}>
+              <Button
+                type="submit"
+                disabled={isSubmitting}
+                variant="contained"
+                color="primary"
+              > Registrarme </Button>
+            </Grid>
+          </Grid>
+        </Form>
+      )}
+    </Formik>
   )
-
-  return <>{form}</>
 }
 
 export default Register
