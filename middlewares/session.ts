5963bf4c61b0ed768541bc8b67c10204ac93fb39
danielmateoslab <danielmateoslab@github.com>
2020-05-01 13:20:07 +0200

    Use express-session instead of next-session

--- middlewares/session.ts
+++ middlewares/session.ts
@@ -1,17 +1,26 @@
 import connectMongo from 'connect-mongo'
-import { session, promisifyStore, Store, MemoryStore } from 'next-session'
-import { NextApiRequest, NextApiResponse } from 'next'
+import session from 'express-session'
+import nextConnect from 'next-connect'
+import onError from './onError'
 
-// @ts-ignore
-const MongoStore = connectMongo({ Store, MemoryStore })
+const MongoStore = connectMongo(session)
 
-export default function(req: NextApiRequest, res: NextApiResponse, next: Function) {
-  const mongoStore = new MongoStore({
-    client: req.dbClient,
-    dbName: process.env.DB_NAME,
-    stringify: false
-  })
-  return session({
-    store: promisifyStore(mongoStore)
-  })(req, res, next)
-}
+const store = new MongoStore({
+  url: process.env.DB_URI!
+})
+const secret = process.env.SESS_SECRET!
+const maxAge = process.env.SESS_MAX_AGE ? +process.env.SESS_MAX_AGE : 60 * 60 * 24
+
+const handler = nextConnect({ onError })
+handler.use(session({
+  store,
+  secret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: true,
+    maxAge
+  }
+}) as any)
+
+export default handler
