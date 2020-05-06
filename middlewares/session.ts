2ecaf2985e6fd116ead37ca2d660a0e29c81074c
danielmateoslab <danielmateoslab@github.com>
2020-05-06 17:31:58 +0200

    Re-use client

--- middlewares/session.ts
+++ middlewares/session.ts
@@ -2,17 +2,26 @@ import connectMongo from 'connect-mongo'
 import session from 'express-session'
 import nextConnect from 'next-connect'
 import onError from './onError'
+ import { DbUnavailableError } from '../utils/errors'
 
 const MongoStore = connectMongo(session)
-
-const store = new MongoStore({
-  url: process.env.DB_URI!
-})
+let store: connectMongo.MongoStore
 const secret = process.env.SESS_SECRET!
-const maxAge = process.env.SESS_MAX_AGE ? +process.env.SESS_MAX_AGE : (1000 * 60 * 60 * 24)
+const maxAge = process.env.SESS_MAX_AGE ? +process.env.SESS_MAX_AGE : (1000 * 60 * 60 * 24) // One day
 
 const handler = nextConnect({ onError })
+
+handler.use((req, res, next) => {
+  store = new MongoStore({ client: req.dbClient })
+    .on('error', (error: Error) => {
+      throw new DbUnavailableError(error.message)
+    })
+
+  next()
+})
+
 handler.use(session({
+// @ts-ignore
   store,
   secret,
   resave: false,
