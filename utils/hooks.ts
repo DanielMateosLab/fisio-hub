3fbe22d284df60d0abe6da0100d2b8b498d18843
danielmateoslab <danielmateoslab@github.com>
2020-05-13 11:06:10 +0200

    Should be revised. Do not want to waste time on this as I might use redux for state management.

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -8,12 +8,14 @@ export function useUser() {
   const { data, mutate } = useSWR('/api/user', fetcher, {
     shouldRetryOnError: false
   })
+  // TODO: the user is not re-sent whenever it changes
   const user: Professional = data && data.professional
   return { user, mutate }
 }
 
 export function useGuestOnly() {
   const { user } = useUser()
+  // Changes in user do not trigger a Router.back()
 
   if (user) {
     Router.back()
