f0b969967460df95a578fdbb5cef7cfad2ec1391
danielmateoslab <danielmateoslab@github.com>
2020-05-14 14:13:07 +0200

    Created users and centers injection middleware

--- middlewares/collections.ts
+++ middlewares/collections.ts
@@ -1,7 +1,19 @@
 import { Middleware } from '../utils/types'
 import ProfessionalsDAO from '../storage/professionalsDAO'
+import usersDAO from '../storage/usersDAO'
+import centersDAO from '../storage/centersDAO'
 
 export const professionals: Middleware = (req, res, next) => {
-  ProfessionalsDAO.injectDB(req.db)
+  ProfessionalsDAO.injectDB(req.dbClient)
+  next()
+}
+
+export const users: Middleware = (req, res, next) => {
+  usersDAO.injectDB(req.db)
+  next()
+}
+
+export const centers: Middleware = (req, res, next) => {
+  centersDAO.injectDB(req.db)
   next()
 }
