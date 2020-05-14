e324e6790346d6b164d1e512554461b45acaef00
danielmateoslab <danielmateoslab@github.com>
2020-05-14 14:12:39 +0200

    passport.ts uses usersDAO

--- utils/passport.ts
+++ utils/passport.ts
@@ -1,33 +1,31 @@
 import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
-import { ObjectID } from 'mongodb'
+import { ObjectId } from 'mongodb'
 import bcrypt from 'bcryptjs'
-import { Professional } from '../storage/professionalsDAO'
+import { User } from '../storage/types'
 import { NextApiRequest } from 'next'
 import { LoginError } from './errors'
+import UsersDAO from '../storage/usersDAO'
 
-
-passport.serializeUser<Professional, string>((user, done) => {
+passport.serializeUser<User, string>((user, done) => {
   done(null, user._id!.toString())
 })
 
-passport.deserializeUser<Professional, string, NextApiRequest>((req, id, done: Function) => {
-  req.db
-    .collection('professionals')
-    .findOne(new ObjectID(id))
-    .then((user: Professional) => {
-      done(null, user)
-    })
+passport.deserializeUser<User, string, NextApiRequest>((req, id, done) => {
+  UsersDAO.injectDB(req.db)
+  UsersDAO.getUserById(new ObjectId(id))
+    .then(user => user && done(null, user))
 })
 
 passport.use(new LocalStrategy(
   { usernameField: 'email', passReqToCallback: true },
   async (req, email, password, done) => {
     try {
-      const user = await req.db.collection('professionals').findOne({ email })
-      const isValidPassword = await bcrypt.compare(password, user.password)
+      UsersDAO.injectDB(req.db)
+      const user = await UsersDAO.getUserByEmail(email)
+      const isValidPassword = user && user.password && await bcrypt.compare(password, user.password)
 
-      if (user && isValidPassword) {
+      if (isValidPassword) {
         done(null, user)
       } else {
         done(new LoginError(), false)
