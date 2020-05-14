6e52be7a6916d470401113af5c26099a6aa6c0d8
danielmateoslab <danielmateoslab@github.com>
2020-05-14 13:38:55 +0200

    Create centersDAO.ts

---
+++ storage/centersDAO.ts
@@ -0,0 +1,21 @@
+import { Collection, Db, ObjectId } from 'mongodb'
+import { Center } from './types'
+
+let centers: Collection<Center>
+
+export default class UsersDAO {
+  static injectDB(db: Db): void {
+    if (centers) {
+      return
+    }
+    centers = db.collection('centers')
+  }
+
+  static async getCenterById(_id: ObjectId) {
+    return await centers.findOne({ _id })
+  }
+
+  static async createCenter(center: Center) {
+    return await centers.insertOne(center).then(writeOpResult => writeOpResult.ops[0])
+  }
+}
---
+++ test/storage/centersDAO.test.ts
@@ -0,0 +1,46 @@
+import { Collection, MongoClient } from 'mongodb'
+import { Center } from '../../storage/types'
+import usersDAO from '../../storage/usersDAO'
+import centersDAO from '../../storage/centersDAO'
+
+require('dotenv').config()
+
+describe('usersDAO', () => {
+  let client: MongoClient
+  let centers: Collection<Center>
+  let mockCenter: Center
+
+  beforeAll(async () => {
+    client = await MongoClient.connect(process.env.DB_URI!, {
+      useNewUrlParser: true, useUnifiedTopology: true })
+    const db = client.db('test')
+    centers = db.collection('centers')
+    centersDAO.injectDB(db)
+  })
+  beforeEach(() => {
+    mockCenter = require('../testUtils').mockCenter
+  })
+  afterEach(async () => {
+    await centers.deleteMany({})
+  })
+  afterAll(async () => {
+    await client.close()
+  })
+
+  describe('createCenter', () => {
+    it('should return the created center', async () => {
+      const center = await centersDAO.createCenter(mockCenter)
+
+      expect(center).toEqual(mockCenter)
+    })
+  })
+
+  describe('getCenter', () => {
+    it('should return a center', async () => {
+      await centers.insertOne(mockCenter)
+      const result = await centersDAO.getCenterById(mockCenter._id!)
+
+      expect(result).toEqual(mockCenter)
+    })
+  })
+})
