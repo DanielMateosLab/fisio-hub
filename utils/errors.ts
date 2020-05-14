ee1d3c17eb8a408f13961c44319ea4ab8a478e81
danielmateoslab <danielmateoslab@github.com>
2020-05-14 13:40:44 +0200

    Created NotFoundError

--- utils/errors.ts
+++ utils/errors.ts
@@ -6,6 +6,17 @@ export class LoginError {
   constructor() {}
 }
 
+export class NotFoundError {
+  status = 404
+  message = 'No se ha encontrado el recurso solicitado'
+
+  constructor(message?: string) {
+    if (message) {
+      this.message = message
+    }
+  }
+}
+
 export class ServiceUnavailableError {
   status = 503
   message = 'El servidor no puede resolver la solicitud en este momento'
