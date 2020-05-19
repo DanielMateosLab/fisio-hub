fb2059d27e2bd448436e8c2e73de8bc5ed727be1
danielmateoslab <danielmateoslab@github.com>
2020-05-19 10:56:01 +0200

    Use next-session instead of express-session

--- middlewares/session.ts
+++ middlewares/session.ts
@@ -1,35 +1,22 @@
+import { MemoryStore, promisifyStore, session, Store } from 'next-session'
 import connectMongo from 'connect-mongo'
-import session from 'express-session'
-import nextConnect from 'next-connect'
-import onError from './onError'
- import { DbUnavailableError } from '../utils/errors'
+import { RequestHandler } from '../utils/types'
 
-const MongoStore = connectMongo(session)
-let store: connectMongo.MongoStore
-const secret = process.env.SESS_SECRET!
-const maxAge = process.env.SESS_MAX_AGE ? +process.env.SESS_MAX_AGE : (1000 * 60 * 60 * 24) // One day
+const MongoStore = connectMongo({ Store, MemoryStore } as any)
+const maxAge = process.env.SESS_MAX_AGE ? +process.env.SESS_MAX_AGE : (60 * 60 * 24) // One day
 
-const handler = nextConnect({ onError })
+const sessionMiddleware: RequestHandler = (req, res, next) => {
+  const mongoStore = new MongoStore({
+    client: req.dbClient,
+    stringify: false,
+  })
 
-handler.use((req, res, next) => {
-  store = new MongoStore({ client: req.dbClient })
-    .on('error', (error: Error) => {
-      throw new DbUnavailableError(error.message)
-    })
+  return session({
+    store: promisifyStore(mongoStore),
+    cookie: {
+      maxAge
+    }
+  })(req, res, next)
+}
 
-  next()
-})
-
-handler.use(session({
-// @ts-ignore
-  store,
-  secret,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    sameSite: true,
-    maxAge
-  }
-}) as any)
-
-export default handler
+export default sessionMiddleware
