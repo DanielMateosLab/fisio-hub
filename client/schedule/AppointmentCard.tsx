5d975d7431d3c512a34f6e83b52f70bcfc15fca4
danielmateoslab <danielmateoslab@github.com>
2020-07-01 16:08:48 +0200

    Use ResizeObserver to make AppointmentCard reactive

--- client/schedule/AppointmentCard.tsx
+++ client/schedule/AppointmentCard.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Appointment } from '../../common/entityTypes'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import { Euro } from '@material-ui/icons'
+import { Euro, Message } from '@material-ui/icons'
 import { brown, green, teal } from '@material-ui/core/colors'
 import moment from 'moment'
 import makeStyles from '@material-ui/core/styles/makeStyles'
@@ -37,18 +37,47 @@ const AppointmentCard: React.FC<Props> = ({ appointment }) => {
 
   const { date, patient, service, payment } = appointment
 
+  let patientName = patient.firstName + ' ' + patient.lastName
+  if (patientName.length >= 30) {
+    const words = patientName.split(' ')
+    words.pop()
+    patientName = words.join(' ')
+  }
+
+  const [ largeCard, setLargeCard ] = useState(false)
+
+  const card = React.createRef<HTMLDivElement>()
+  const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      entry.contentRect.width >= 645 ? setLargeCard(true) : setLargeCard(false)
+    }
+  })
+  useEffect(() => {
+    if (!card.current) return
+    resizeObserver.observe(card.current)
+    return () => resizeObserver.disconnect()
+  }, [])
+
   return (
-    <Paper className={classes.root}>
-      <Grid container style={{ height: '100%' }}>
-        <Grid item>
-          <Typography color="textSecondary" variant="caption">
-            { moment(date).format("HH:mm") } {' '} { service.name }
-          </Typography>
-          <Typography color="primary" variant="subtitle2">{ `${patient.firstName} ${patient.lastName}` }</Typography>
+    <Paper className={classes.root} ref={card}>
+      <Grid container style={{ height: '100%' }} spacing={2}>
+        <Grid item container>
+          <Grid item xs={12} container alignItems="center">
+            <Typography color="textSecondary" variant="caption">
+              { moment(date).format("HH:mm") } {' '} { service.name }
+            </Typography>
+            <Grid item xs />
+            <Message fontSize="small" />
+            { payment && <Euro style={{ color: green['500'] }} fontSize="small"/> }
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="primary" variant={ largeCard ? 'h5' : 'subtitle2' }>{ 'a'.padEnd(25, 'b') }</Typography>
+          </Grid>
         </Grid>
-        { payment && (
-          <Grid item xs container alignItems="center" justify="center">
-            <Euro style={{ color: green['500'] }} fontSize="small"/>
+        { largeCard && (
+          <Grid item xs container alignItems="center">
+            <Message fontSize="large"/>
+            { payment && <Euro style={{ color: green['500'] }} fontSize="large"/> }
           </Grid>
         ) }
       </Grid>
