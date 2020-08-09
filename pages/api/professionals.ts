808fb4623ee609fb60ac477d2f6795f447a16a35
danielmateoslab <danielmateoslab@github.com>
2020-08-09 17:33:00 +0200

    Comment unused lines

--- pages/api/professionals.ts
+++ pages/api/professionals.ts
@@ -1,11 +1,8 @@
-import ProfessionalsDAO from "../../server/storage/professionalsDAO";
-import { centerValidationSchema } from '../../common/validation'
 import nextConnect from 'next-connect'
 import middleware from '../../server/middlewares/middleware'
 import { centers, professionals, users } from '../../server/middlewares/collections'
 import onError from '../../server/middlewares/onError'
 import { Professional } from '../../common/entityTypes'
-import { NextApiResponse } from 'next'
 
 export type ProfessionalsResponseData = { professional: Professional } & { professionals: Professional[] }
 
@@ -13,20 +10,20 @@ const handler = nextConnect({ onError })
 
 handler.use(middleware, professionals, users, centers)
 
-handler.post(async (req, res: NextApiResponse<ProfessionalsResponseData>, next) => {
-  try {
-    const validProfessional = await centerValidationSchema.validate(req.body, { abortEarly: false })
-    delete validProfessional.repeatPassword
-
-    const professional = await ProfessionalsDAO.addProfessional(validProfessional)
-    req.logIn(professional, err => {
-      if (err) throw err
-    })
-
-    res.status(201).json({ status: 'success', professional: '' })
-  } catch (e) {
-    next(e)
-  }
-})
+// handler.post(async (req, res: NextApiResponse<ProfessionalsResponseData>, next) => {
+//   try {
+//     const validProfessional = await centerValidationSchema.validate(req.body, { abortEarly: false })
+//     delete validProfessional.repeatPassword
+//
+//     const professional = await ProfessionalsDAO.addProfessional(validProfessional)
+//     req.logIn(professional, err => {
+//       if (err) throw err
+//     })
+//
+//     res.status(201).json({ status: 'success', professional: '' })
+//   } catch (e) {
+//     next(e)
+//   }
+// })
 
 export default handler
